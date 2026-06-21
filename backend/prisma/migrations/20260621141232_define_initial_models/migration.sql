1bc67adfac1c3a47c1e72884c153c55311326bcc
WAnubhavGoel <goelanubhav70@gmail.com>
2026-06-21 15:13:57 +0000

    Add routes structure.

---
+++ backend/prisma/migrations/20260621141232_define_initial_models/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "membership" BOOLEAN NOT NULL DEFAULT false,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import passport from "./session-store/passport.js";
 import { sessionMiddleware } from "./session-store/session.js";
+import { indexRouter } from "./routes/index.js";
+import { userRouter } from "./routes/user.js";
+import { messageRouter } from "./routes/message.js";
+import { authRouter } from "./routes/auth.js";
+
 const app=express();
+
 app.use(sessionMidlleware);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 app.use(passport.session());
+app.use("/api",indexRouter);
+app.use("/api/auth",authRouter);
+app.use("/api/users",userRouter);
+app.use("/api/messages",messageRouter);
+
 const PORT=3000||process.env.PORT;
+
 app.listen(PORT,(error)=>{
     if(error){
         console.log(error);
